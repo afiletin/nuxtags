7b8cce63f9e6724dc5dd5e06ce5e0ce8de15de16
afiletin <119939166+afiletin@users.noreply.github.com>
2023-02-19 11:03:03 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,32 @@
 <template>
-    <div class="text-3xl font-bold text-center py-16">
-      <h2 class="one">Nuxt 3</h2>
-      <p class="text-gray-400">+</p>
-      <h2 class="two">Tailwind CSS</h2>
-      <p class="text-gray-400">+</p>
-      <h2 class="three">GSAP</h2>
-    </div>
-  </template>
+  <div class="text-3xl font-bold text-center py-16">
+    <h2 class="one">Nuxt 3</h2>
+    <p class="text-gray-400">+</p>
+    <h2 class="two">Tailwind CSS</h2>
+    <p class="text-gray-400">+</p>
+    <h2 class="three">GSAP</h2>
+    <button class="p-3 mt-6 bg-black text-white" @click="clickAnimations">Animate</button>
+  </div>
+</template>
   
-  <script setup>
-  import gsap from 'gsap';
+<script>
+  import gsap from 'gsap'
   
-  onMounted(() => {
-    gsap.to('.one', { rotation: 360, duration: 1, ease: "bounce.out" }),
-    gsap.to('.two', { rotation: 360, duration: 1, delay: 0.5, ease: "bounce.out" }),
-    gsap.to('.three', { rotation: 360, duration: 1, delay: 1, ease: "bounce.out" })
-  });
-  
-  </script>
+  export default {
+    mounted: function() {
+      this.startAnimations()
+    },
+    methods: {
+      startAnimations: function() {
+        gsap.to('.one', { rotation: 360, duration: 1, ease: "bounce.out" }),
+        gsap.to('.two', { rotation: 360, duration: 1, delay: 0.5, ease: "bounce.out" }),
+        gsap.to('.three', { rotation: 360, duration: 1, delay: 1, ease: "bounce.out" })
+      },
+      clickAnimations: function() {
+        gsap.to('.one', { rotation: 0, duration: 1, ease: "bounce.out" }),
+        gsap.to('.two', { rotation: 0, duration: 1, delay: 0.5, ease: "bounce.out" }),
+        gsap.to('.three', { rotation: 0, duration: 1, delay: 1, ease: "bounce.out" })
+      }
+    }
+  }
+</script>
